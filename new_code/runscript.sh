d1ad774949b38dc6b8ec893fe81198a801735fb2
RGologorsky <rgologorsky@college.harvard.edu>
2019-02-04 17:51:22 -0500

    short

--- new_code/runscript.sh
+++ new_code/runscript.sh
@@ -2,11 +2,13 @@
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
 #SBATCH -t 0-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p general   # Partition to submit to
+#SBATCH -p test   # Partition to submit to
 #SBATCH --mem=100           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --test-only
 
 module load Anaconda3/5.0.1-fasrc02 #Load Perl module
-python3 'how_many_timesteps.py'
+source activate thesis_env
+cd thesis_stuff/ipd_trust/new_code
+python3 'short.py'

055d7580e327de1a7e0d51bcec2307a0d0333135
RGologorsky <rgologorsky@college.harvard.edu>
2019-02-04 18:48:42 -0500

    shotime

--- new_code/runscript.sh
+++ new_code/runscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 1                # Number of cores
 #SBATCH -N 1                # Ensure that all cores are on one machine
-#SBATCH -t 0-00:10          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 1-06:10          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p general   # Partition to submit to
 #SBATCH --mem=100           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
